a38f10c4fd0a844fbdd602b2906f14126ba6fc55
Byron <3018080310101@ingenieria.usac.edu.gt>
2021-03-21 02:54:38 -0600

    Agregados modulos para frontend

--- Frontend/Typesty/src/app/app-routing.module.ts
+++ Frontend/Typesty/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArbolASTComponent } from './paginas/arbol-ast/arbol-ast.component';
+import { ErroresComponent } from './paginas/errores/errores.component';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { TablaSimbolosComponent } from './paginas/tabla-simbolos/tabla-simbolos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'errores',component: ErroresComponent},
+  {path: 'ast',component: ArbolASTComponent},
+  {path: 'inicio',component: InicioComponent},
+  {path: 'simbolos',component: TablaSimbolosComponent},
+  {path: '', component: InicioComponent},
+  {path: '**', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
