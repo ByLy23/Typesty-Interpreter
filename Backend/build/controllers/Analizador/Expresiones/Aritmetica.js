11f0046c8c9752bba2fabe4e7bf770e74ad97ae6
Byron <3018080310101@ingenieria.usac.edu.gt>
2021-04-08 15:48:57 -0600

    Asignacion datos a la tabla

--- Backend/build/controllers/Analizador/Expresiones/Aritmetica.js
+++ Backend/build/controllers/Analizador/Expresiones/Aritmetica.js
@@ -1,2 +1,87 @@
 "use strict";
-//primero
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Operadores = void 0;
+var Instruccion_1 = require("../Abastracto/Instruccion");
+var Errores_1 = __importDefault(require("../Excepciones/Errores"));
+var Tipo_1 = __importStar(require("../Simbolos/Tipo"));
+var Aritmetica = /** @class */ (function (_super) {
+    __extends(Aritmetica, _super);
+    function Aritmetica(operador, fila, columna, op1, op2) {
+        var _this = _super.call(this, new Tipo_1.default(Tipo_1.tipoDato.ENTERO), fila, columna) || this;
+        _this.operador = operador;
+        if (!op2)
+            _this.operandoUnico = _this.operando1;
+        else {
+            _this.operando1 = op1;
+            _this.operando2 = op2;
+        }
+        return _this;
+    }
+    Aritmetica.prototype.interpretar = function (arbol, tabla) {
+        var _a, _b;
+        var izq, der, uno;
+        izq = der = uno = null;
+        if (this.operandoUnico != null) {
+            uno = this.operandoUnico.interpretar(arbol, tabla);
+            if (uno instanceof Errores_1.default)
+                return uno;
+        }
+        else {
+            izq = (_a = this.operando1) === null || _a === void 0 ? void 0 : _a.interpretar(arbol, tabla);
+            if (izq instanceof Errores_1.default)
+                return izq;
+            der = (_b = this.operando2) === null || _b === void 0 ? void 0 : _b.interpretar(arbol, tabla);
+            if (der instanceof Errores_1.default)
+                return der;
+        }
+    };
+    return Aritmetica;
+}(Instruccion_1.Instruccion));
+exports.default = Aritmetica;
+var Operadores;
+(function (Operadores) {
+    Operadores[Operadores["SUMA"] = 0] = "SUMA";
+    Operadores[Operadores["RESTA"] = 1] = "RESTA";
+    Operadores[Operadores["MULTIPLICACION"] = 2] = "MULTIPLICACION";
+    Operadores[Operadores["DIVISION"] = 3] = "DIVISION";
+    Operadores[Operadores["POTENCIA"] = 4] = "POTENCIA";
+    Operadores[Operadores["MODULADOR"] = 5] = "MODULADOR";
+    Operadores[Operadores["MENOSNUM"] = 6] = "MENOSNUM";
+})(Operadores = exports.Operadores || (exports.Operadores = {}));
--- Backend/src/controllers/Analizador/Expresiones/Aritmetica.ts
+++ Backend/src/controllers/Analizador/Expresiones/Aritmetica.ts
@@ -1,1 +1,49 @@
-//primero
+import { table } from "node:console";
+import { Instruccion } from "../Abastracto/Instruccion";
+import Errores from "../Excepciones/Errores";
+import Arbol from "../Simbolos/Arbol";
+import tablaSimbolos from "../Simbolos/tablaSimbolos";
+import Tipo, { tipoDato } from "../Simbolos/Tipo";
+
+
+
+
+export default class Aritmetica extends Instruccion{
+    private operando1:Instruccion|undefined;
+    private operando2:Instruccion|undefined;
+    private operandoUnico:Instruccion|undefined;
+    private operador:Operadores;
+
+    constructor(operador:Operadores, fila:Number, columna:Number,op1:Instruccion,op2?:Instruccion){
+        super(new Tipo(tipoDato.ENTERO),fila,columna);
+        this.operador=operador;
+        if(!op2)
+            this.operandoUnico=this.operando1;
+        else{
+            this.operando1=op1;
+            this.operando2=op2;
+        }
+    }
+    public interpretar(arbol:Arbol,tabla:tablaSimbolos){
+        let izq,der,uno;
+        izq=der=uno=null;
+        if(this.operandoUnico!=null){
+            uno=this.operandoUnico.interpretar(arbol,tabla);
+            if(uno instanceof Errores) return uno;
+        }else{
+            izq=this.operando1?.interpretar(arbol,tabla);
+            if(izq instanceof Errores) return izq;
+            der=this.operando2?.interpretar(arbol,tabla);
+            if(der instanceof Errores) return der;
+        }
+    }
+}
+export enum Operadores{
+    SUMA,
+    RESTA,
+    MULTIPLICACION,
+    DIVISION,
+    POTENCIA,
+    MODULADOR,
+    MENOSNUM
+}
