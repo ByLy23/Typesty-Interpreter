f93faeda5a7f4d9daafa675073f4da47baee0c56
Byron <3018080310101@ingenieria.usac.edu.gt>
2021-04-10 03:37:36 -0600

    Inicio relacionales

--- Backend/build/controllers/Analizador/Expresiones/Relacional.js
+++ Backend/build/controllers/Analizador/Expresiones/Relacional.js
@@ -1,2 +1,78 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Relacionales = void 0;
 //aritmeticas
+var Instruccion_1 = require("../Abastracto/Instruccion");
+var Errores_1 = __importDefault(require("../Excepciones/Errores"));
+var Tipo_1 = __importStar(require("../Simbolos/Tipo"));
+var Relacional = /** @class */ (function (_super) {
+    __extends(Relacional, _super);
+    function Relacional(relacion, fila, columna, cond1, cond2) {
+        var _this = _super.call(this, new Tipo_1.default(Tipo_1.tipoDato.ENTERO), fila, columna) || this;
+        _this.relacion = relacion;
+        _this.cond1 = cond1;
+        _this.cond2 = cond2;
+        return _this;
+    }
+    Relacional.prototype.interpretar = function (arbol, tabla) {
+        var izq, der;
+        izq = this.cond1.interpretar(arbol, tabla);
+        if (izq instanceof Errores_1.default)
+            return izq;
+        der = this.cond2.interpretar(arbol, tabla);
+        if (der instanceof Errores_1.default)
+            return der;
+        switch (this.relacion) {
+            case Relacionales.IGUAL:
+                return "a";
+        }
+    };
+    return Relacional;
+}(Instruccion_1.Instruccion));
+exports.default = Relacional;
+var Relacionales;
+(function (Relacionales) {
+    Relacionales[Relacionales["IGUAL"] = 0] = "IGUAL";
+    Relacionales[Relacionales["DIFERENTE"] = 1] = "DIFERENTE";
+    Relacionales[Relacionales["MAYOR"] = 2] = "MAYOR";
+    Relacionales[Relacionales["MENOR"] = 3] = "MENOR";
+    Relacionales[Relacionales["MAYORIGUAL"] = 4] = "MAYORIGUAL";
+    Relacionales[Relacionales["MENORIGUAL"] = 5] = "MENORIGUAL";
+})(Relacionales = exports.Relacionales || (exports.Relacionales = {}));
--- Backend/src/controllers/Analizador/Expresiones/Relacional.ts
+++ Backend/src/controllers/Analizador/Expresiones/Relacional.ts
@@ -1,1 +1,44 @@
 //aritmeticas
+import { Instruccion } from "../Abastracto/Instruccion";
+import Errores from "../Excepciones/Errores";
+import Arbol from "../Simbolos/Arbol";
+import tablaSimbolos from "../Simbolos/tablaSimbolos";
+import Tipo, { tipoDato } from "../Simbolos/Tipo";
+
+export default class Relacional extends Instruccion {
+  private cond1: Instruccion;
+  private cond2: Instruccion;
+  private relacion: Relacionales;
+  constructor(
+    relacion: Relacionales,
+    fila: Number,
+    columna: Number,
+    cond1: Instruccion,
+    cond2: Instruccion
+  ) {
+    super(new Tipo(tipoDato.ENTERO), fila, columna);
+    this.relacion = relacion;
+    this.cond1 = cond1;
+    this.cond2 = cond2;
+  }
+  public interpretar(arbol: Arbol, tabla: tablaSimbolos) {
+    let izq, der;
+    izq = this.cond1.interpretar(arbol, tabla);
+    if (izq instanceof Errores) return izq;
+    der = this.cond2.interpretar(arbol, tabla);
+    if (der instanceof Errores) return der;
+    switch (this.relacion) {
+      case Relacionales.IGUAL:
+        return "a";
+    }
+  }
+}
+
+export enum Relacionales {
+  IGUAL,
+  DIFERENTE,
+  MAYOR,
+  MENOR,
+  MAYORIGUAL,
+  MENORIGUAL,
+}
